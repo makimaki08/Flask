c28b3d35a8e36806e55961b4a551cac5b93887a5
makimaki08 <08.kobayashi@gmail.com>
2023-07-28 20:42:35 +0900

    for文で情報を渡す処理を追加

--- testapp/views.py
+++ testapp/views.py
@@ -5,7 +5,8 @@ from testapp import app
 def index():
     my_dict = {
         'insert_something1': 'views.pyのinsert_something1部分です',
-        'insert_something2': 'views.pyのinsert_something2部分です'
+        'insert_something2': 'views.pyのinsert_something2部分です',
+        'test_titles': ['title1', 'title2', 'title3']
     }
     return render_template('testapp/index.html', my_dict=my_dict)
 
