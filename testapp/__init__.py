94253a04c857deec752fa01376252c8089445c71
makimaki08 <08.kobayashi@gmail.com>
2023-08-03 21:41:09 +0900

    ターミナルでPythonからSQLAlchemyメソッドを実行できるように、__init__.pyを変更

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('testapp.config')
 
 db = SQLAlchemy(app)
+from .models import employee
 
 import testapp.views
 
